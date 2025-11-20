d5e732d1cfd6eec714faf37eef8db75977c45fda
Yerssi21 <111612832+Yerssi21@users.noreply.github.com>
2025-11-20 18:36:05 +0100

    feat: Se Mejora el diseño de Soporte Empresarial, Mini-FAQs (2–3 preguntas típicas).

--- src/app/products-module/soporte-empresarial/soporte-empresarial.component.ts
+++ src/app/products-module/soporte-empresarial/soporte-empresarial.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-soporte-empresarial',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './soporte-empresarial.component.html',
-  styleUrl: './soporte-empresarial.component.scss'
+  styleUrls: ['./soporte-empresarial.component.scss'],
 })
 export class SoporteEmpresarialComponent {
 
